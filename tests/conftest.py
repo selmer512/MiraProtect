f235aab067ddf5b276b99ae84e1cb8e7f6cde506
Cyberhoney <99543386+selmer512@users.noreply.github.com>
2026-09-20 11:46:23 -0400

    Keep endpoint enrollment tests environment-independent

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,11 @@ import os
 os.environ.setdefault("MIRA_DATABASE_URL", "sqlite+pysqlite:///:memory:")
 os.environ.pop("MIRA_ENDPOINT_TOKEN", None)
 os.environ.pop("MIRA_ENDPOINT_DENY_PROCESSES", None)
+
+os.environ.pop("MIRA_ENROLLMENT_TOKEN", None)
+os.environ.pop("MIRA_ALLOW_SHARED_ENDPOINT_TOKEN", None)
+os.environ.pop("MIRA_TOKEN_PEPPER", None)
+os.environ.pop("MIRA_ENDPOINT_PROCESS_NAMES", None)
+os.environ.pop("MIRA_ENDPOINT_COMMAND_MARKERS", None)
+os.environ.pop("MIRA_ENDPOINT_FAIL_CLOSED", None)
+os.environ.pop("MIRA_ENDPOINT_POLICY_MODE", None)
